f4eb051e4af5093bad39b8058adff17be7ed53bf
Ladhwe <sladhwe@dxc.com>
2024-05-30 03:53:17 -0700

    first commit

---
+++ src/Tests/LoginPageTests.java
@@ -0,0 +1,13 @@
+package Tests;
+
+public class LoginPageTests {
+	public void test1()
+{
+			System.out.println("Hello");
+		}
+
+	}
+
+
+
+
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Git_Practice1 {
+}
