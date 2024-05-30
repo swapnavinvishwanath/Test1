b4448062ebe7abc3223c47cfd39884a90a08d28e
Ladhwe <sladhwe@dxc.com>
2024-05-30 10:39:09 -0700

    added/deleted/updated

--- src/Pages/LoginPage.java
+++ src/Pages/LoginPage.java
@@ -5,6 +5,9 @@ public class LoginPage {
 public static void main(String a[])
 {
 	System.out.println("Hello");
+	String un = "admin";
+	String pwd = "admin";
+	System.out.println("Logged in....");
 }
 
 }
