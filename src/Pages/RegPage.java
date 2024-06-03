eb55ceb88a0d3cc7c6cc4bfd22487a904a32ed4f
Ladhwe <sladhwe@dxc.com>
2024-06-03 00:22:14 -0700

    updated/added

---
+++ src/Pages/RegPage.java
@@ -0,0 +1,12 @@
+package Pages;
+
+public class RegPage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 1;
+		
+
+	}
+
+}
---
+++ src/Tests/MyTest.java
@@ -0,0 +1,11 @@
+package Tests;
+
+public class MyTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = "Test";
+
+	}
+
+}
