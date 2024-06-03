53680df40347f7ca3326b2a035c4f65a373b1a41
Ladhwe <sladhwe@dxc.com>
2024-06-03 11:12:37 -0700

    added

--- src/Pages/RegPage.java
+++ src/Pages/RegPage.java
@@ -5,6 +5,8 @@ public class RegPage {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int i = 1;
+		String city = "New York";
+		String country = "USA";
 		System.out.println("Registration page");
 		
 
